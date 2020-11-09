ddab130236b3373d0ded3de578ec94882d93656a
Ronnie Barker <ronnie.barker@theagileworkshop.com>
2020-11-09 13:06:32 +0000

    Added a form with a summery (like the weather but unrelated) which, if renamed through a refactoring will break

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
-@Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-})
-export class HomeComponent {
+
+
+@Component( {
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+} )
+export class HomeComponent implements OnInit
+{
+  private form: FormGroup;
+
+
+
+  constructor( private fb: FormBuilder )
+  {
+  }
+
+
+
+  public ngOnInit(): void
+  {
+    this.form = this.fb.group(
+        {
+          id: [],
+          amount: [],
+          discount: [],
+          summary: [],
+        },
+    );
+  }
 }
